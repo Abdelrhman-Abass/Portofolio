dc6dc6bc525ba98dc183426092eaf01be3d3ae8b
Abdelrahman_Abass <92057185+Abdelrhman-Abass@users.noreply.github.com>
2022-12-19 12:19:56 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+
+	
+	/* Navigation burger onclick side navigation show */
+	$('.burger-container').on('click', function() {
+		$('.main-navigation').toggle('slow');
+
+		if($('#myBtn').hasClass('change')) {
+			$('body').addClass('stop-scroll');
+		} else {
+			$('body').removeClass('stop-scroll');
+		}
+	});
+
+
+	/* About me slider */
+	$('.about-me-slider').slick({
+		slidesToShow: 1,
+		prevArrow: '<span class="span-arrow slick-prev"><</span>',
+		nextArrow: '<span class="span-arrow slick-next">></span>'
+	});
+
+	/* Blog slider */
+	$('.blog-slider').slick({
+		slidesToShow: 2,
+		prevArrow: '<span class="span-arrow slick-prev"><</span>',
+		nextArrow: '<span class="span-arrow slick-next">></span>',
+		responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1
+			}
+		}
+		]
+	});
+	
+});
+
+
+$(document).ready(function() {
+	$(window).scroll(function() {
+	  if ($(this).scrollTop() > 20) {
+		$('#toTopBtn').fadeIn();
+	  } else {
+		$('#toTopBtn').fadeOut();
+	  }
+	});
+  
+	$('#toTopBtn').click(function() {
+	  $("html, body").animate({
+		scrollTop: 0
+	  }, 1000);
+	  return false;
+	});
+  });
+  // define all UI variable
+const navToggler = document.querySelector('.nav-toggler');
+const navMenu = document.querySelector('.site-navbar ul');
+const navLinks = document.querySelectorAll('.site-navbar a');
+
+// load all event listners
+allEventListners();
+
+// functions of all event listners
+function allEventListners() {
+  // toggler icon click event
+  navToggler.addEventListener('click', togglerClick);
+  // nav links click event
+  navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
+}
+
+// togglerClick function
+function togglerClick() {
+  navToggler.classList.toggle('toggler-open');
+  navMenu.classList.toggle('open');
+}
+
+// navLinkClick function
+function navLinkClick() {
+  if(navMenu.classList.contains('open')) {
+    navToggler.click();
+  }
+}
